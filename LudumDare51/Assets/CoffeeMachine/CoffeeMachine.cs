58840c1693b4dc8b115997b8537845513578dc2c
Mister Bamboo <nicolascnaud1989@gmail.com>
2022-10-01 12:53:21 -0400

    Add text "e to pick coffee" when player nearby

---
+++ LudumDare51/Assets/CoffeeMachine/CoffeeMachine.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CoffeeMachine : MonoBehaviour
+{
+    [SerializeField] GameObject[] itemsToShowWhenPlayerNearby;
+    public bool isPlayerNear = false;
+
+    private void Start()
+    {
+        UpdateItemToShow();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SetPlayerNear(other, true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        SetPlayerNear(other, false);
+    }
+
+    private void SetPlayerNear(Collider other, bool state)
+    {
+        if (other.tag == Player.Tag)
+        {
+            isPlayerNear = state;
+            UpdateItemToShow();
+        }
+    }
+
+    private void UpdateItemToShow()
+    {
+        if (itemsToShowWhenPlayerNearby == null) return;
+
+        for (int i = 0; i < itemsToShowWhenPlayerNearby.Length; i++)
+        {
+            itemsToShowWhenPlayerNearby[i].SetActive(isPlayerNear);
+        }
+    }
+}
