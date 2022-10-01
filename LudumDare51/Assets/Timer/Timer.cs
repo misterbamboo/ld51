bbef8296af3d61b8ae2de49779d78f6b94d850be
armenbalian <raphaelfortin@hotmail.com>
2022-09-30 23:32:38 -0500

    move the watch hands

--- LudumDare51/Assets/Timer/Timer.cs
+++ LudumDare51/Assets/Timer/Timer.cs
@@ -11,8 +11,17 @@ public class Timer : MonoBehaviour
     public delegate void SecondPassed();
     public event SecondPassed OnSecondPassed;
 
-    public delegate void TimerDone();
-    public event TimerDone OnTimerDone;
+    public delegate void MinutePassed();
+    public event MinutePassed OnMinutePassed;
+
+    public delegate void HourPassed();
+    public event HourPassed OnHourPassed;
+
+    public delegate void TenSecondsPassed();
+    public event TenSecondsPassed OnTenSecondsPassed;
+
+
+    private int secondsPassed = 0;
 
     // instatiate the singleton
     void Awake()
@@ -34,15 +43,70 @@ public class Timer : MonoBehaviour
 
     IEnumerator CountDown()
     {
-        int time = 10;
-        while (time > 0)
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1);
+            secondsPassed++;
+
+            HandleSecond();
+
+            HandleTenSeconds();
+
+            HandleMinute();
+
+            HandleHour();
+        }
+    }
+
+    private void HandleSecond()
+    {
+        if (OnSecondPassed != null)
         {
             OnSecondPassed();
-            yield return new WaitForSeconds(1);
-            time--;
         }
-        
-        OnTimerDone();
-        StartCoroutine(CountDown());
+    }
+
+    private void HandleMinute()
+    {
+        if (OnMinutePassed == null)
+        {
+            return;
+        }
+
+        var nbSecondInOneMinute = 60;
+        var oneMinuteHavePassed = secondsPassed % nbSecondInOneMinute == 0;
+        if (oneMinuteHavePassed)
+        {
+            OnMinutePassed();
+        }
+    }
+
+    private void HandleHour()
+    {
+        if(OnHourPassed == null)
+        {
+            return;
+        }
+
+        var nbSecondInOneHour = 3600;
+        var oneHourHavePassed = secondsPassed % nbSecondInOneHour == 0;
+        if (oneHourHavePassed)
+        {
+            OnHourPassed();
+        }
+    }
+
+    private void HandleTenSeconds()
+    {
+        if(OnTenSecondsPassed == null)
+        {
+            return;
+        }
+
+        var tenSecondsHavePassed = secondsPassed % 10 == 0;
+        if (tenSecondsHavePassed)
+        {
+            OnTenSecondsPassed();
+        }
     }
 }
