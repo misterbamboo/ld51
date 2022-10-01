8d70cc0938f0bb2dd15eeb1f5faf7ac317dea679
Mister Bamboo <nicolascnaud1989@gmail.com>
2022-09-30 23:40:08 -0400

    Movement with xVelocity and zVelocity splitted

---
+++ LudumDare51/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 velocity;
+    private float accelVelocitySpeed = 2f;
+    private float slowDownVelocitySpeed = 1.5f;
+    private float movementSpeed = 4f;
+
+    void Update()
+    {
+        UpdateVelocity();
+        Move();
+    }
+
+    private void Move()
+    {
+        var directionVector = DirectionVector();
+        if (directionVector != Vector3.zero)
+        {
+            var lastPos = transform.position;
+            var newPos = lastPos + directionVector;
+            transform.position = newPos;
+
+            transform.LookAt(newPos + (newPos - lastPos));
+        }
+    }
+
+    private void UpdateVelocity()
+    {
+        var xInput = Input.GetAxisRaw("Horizontal");
+        var zInput = Input.GetAxisRaw("Vertical");
+
+        var updatedVelocity = velocity;
+        updatedVelocity.x = UpdateVelocity(velocity.x, xInput);
+        updatedVelocity.z = UpdateVelocity(velocity.z, zInput);
+        velocity = updatedVelocity;
+    }
+
+    private Vector3 DirectionVector()
+    {
+        return velocity * movementSpeed * Time.deltaTime;
+    }
+
+    private float UpdateVelocity(float velocity, float input)
+    {
+        var velocitySpeed = accelVelocitySpeed;
+        if (ShouldSlowDown(input))
+        {
+            input = SimulateInvertedInput(velocity);
+            velocitySpeed = slowDownVelocitySpeed;
+        }
+
+        var value = velocity + input * velocitySpeed * Time.deltaTime;
+        return ClampToZero(velocity, value);
+    }
+
+    private static bool ShouldSlowDown(float input) => input == 0;
+
+    private static float SimulateInvertedInput(float velocity) => -Math.Sign(velocity);
+
+    private static float ClampToZero(float velocity, float value)
+    {
+        if (velocity > 0)
+        {
+            value = Mathf.Clamp(value, 0, 1);
+        }
+        if (velocity < 0)
+        {
+            value = Mathf.Clamp(value, -1, 0);
+        }
+
+        return value;
+    }
+}
